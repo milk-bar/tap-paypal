f2ece53257d9cf98541f76024191569231e3b001
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2018-08-08 18:17:55 -0400

    Updating setup.py.

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ from setuptools import setup
 setup(
     name="tap-paypal",
     version="0.1.0",
-    description="Singer.io tap for extracting data",
-    author="Stitch",
-    url="http://singer.io",
+    description="Singer.io tap for extracting data from the PayPal Sync API",
+    author="Josh Temple",
+    url="https://github.com/milk-bar/tap-paypal",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_paypal"],
     install_requires=[
         "singer-python>=5.0.12",
         "requests",
+        "requests_oauthlib",
+        "python-dateutil"
     ],
     entry_points="""
     [console_scripts]
